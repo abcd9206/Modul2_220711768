6bd00932ec504a7615b95d7f30dc79505204a06e
abcd9206 <220711768@students.uajy.ac.id>
2025-04-15 20:46:40 +0700

    Update data_loader.py

--- multilabel/utils/data_loader.py
+++ multilabel/utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset from file
     """
-    df = pd.read_csv('data\train_preprocess.csv')
+    df = pd.read_csv('multilabel\data\train_preprocess.csv')
     return df
