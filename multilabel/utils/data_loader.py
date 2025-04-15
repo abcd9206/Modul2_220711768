6f1fee4dd3d32d39bbfec2b0f009b18001309714
abcd9206 <220711768@students.uajy.ac.id>
2025-04-15 18:02:28 +0700

    Update data_loader.py

--- multilabel/utils/data_loader.py
+++ multilabel/utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset from file
     """
-    df = pd.read_csv('D:\\Kuliah\\Semester 6\\Pengolahan Bahasa Alami\\Modul2_220711768\\multilabel\\data\\train_preprocess.csv')
+    df = pd.read_csv('data\train_preprocess.csv')
     return df
