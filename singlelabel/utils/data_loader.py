3af0200d10f7b3585650cf7b26f16b733dce767e
abcd9206 <220711768@students.uajy.ac.id>
2025-04-15 18:03:03 +0700

    Update data_loader.py

--- singlelabel/utils/data_loader.py
+++ singlelabel/utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset
     """
-    df = pd.read_csv('D:\\Kuliah\\Semester 6\\Pengolahan Bahasa Alami\\Modul2_220711768\\singlelabel\\data\\train_preprocess.csv')
+    df = pd.read_csv('data\train_preprocess.csv')
     return df
